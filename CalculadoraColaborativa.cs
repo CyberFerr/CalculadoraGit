f005c95d33f7b19ee63a4ebe2d05c4796ba8f800
CyberFerr <cyberferr2077@gmail.com>
2023-09-12 14:29:36 -0300

    Tarefa comunitaria colcocar o nome dos integrantes e mcada grupo , cada grupo va ifazer uma implementação embaixo do comentario

---
+++ CalculadoraColaborativa.cs
@@ -0,0 +1,11 @@
+// projeto calculadora comunitaria 
+
+// Proferr  vai inserir  comadno de inputs 
+
+// Grupo 1 implementa a operação de soma 
+
+// Grupo 2 implementa a operaçã de subtração 
+
+// Grupo 3 implementa a operação de multiplicação
+
+// Grupo 4 implementa a operação de divisão
