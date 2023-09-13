f209e3e0aec74a81f4e9ae1469e849ee6e04cea4
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-09-12 23:48:29 -0300

    Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Calculator");
+Console.WriteLine("___________");
+
+Console.Write("\nNumber 1: ");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nNumber 2: ");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("\nOperation (+ - / *): ");
+
+string operation = Console.ReadLine();
+
+switch(operation){
+    case "+":
+        Console.WriteLine("\nResult: " + (number1 + number2));
+        break;
+    case "-":
+        Console.WriteLine("\nResult: " + (number1 - number2));
+        break;
+    case "/":
+        Console.WriteLine("\nResult: " + (number1 / number2));
+        break;
+    case "*":
+        Console.WriteLine("\nResult: " + (number1 * number2));
+        break;
+}
