53319a92cfa77c29773c658d76d17d4bea4018ce
CyberFerr <cyberferr2077@gmail.com>
2023-09-26 13:53:34 -0300

    implementação dso intput e algoritmos de calculo

--- CalculadoraColaborativa.cs
+++ CalculadoraColaborativa.cs
@@ -1,11 +1,28 @@
 // projeto calculadora comunitaria 
 
-// Proferr  vai inserir  comadno de inputs 
+// Proferr  vai inserir  comandTarefa colocao de inputs 
 
-// Grupo 1 implementa a operação de soma 
+// Grupo 1 implementa a operação de soma ( Kauã)
 
-// Grupo 2 implementa a operaçã de subtração 
+// Grupo 2 implementa a operação de subtração ( Matheus Mizuno )
 
-// Grupo 3 implementa a operação de multiplicação
+// Grupo 3 implementa a operação de multiplicação ( Udiel)
 
-// Grupo 4 implementa a operação de divisão
+// O partipante pro - ativo implementa a operação de divisão
+
+class Program {
+  public static void Main (string[] args) {
+    
+  int x = 0 ;
+  int y = 0 ;
+  
+    
+  Console.WriteLine("Entre com o valor de  x : ");
+  x = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Entre com o valor de  y : ");
+  y = Convert.ToInt32(Console.ReadLine());
+  int soma = x + y ;
+  Console.WriteLine("A soma dos valores é igual a " + soma);
+
+  }
+}
