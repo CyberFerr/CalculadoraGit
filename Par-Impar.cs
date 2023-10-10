f26fe5923b1814c0748ea52c73671d487a560a2d
matmizuno <124712609+matmizuno@users.noreply.github.com>
2023-10-10 14:47:29 -0300

    Código Desenvolvido Pelo Grupo

---
+++ Par-Impar.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+              int numero;
+
+   Console.WriteLine("Insira o Valor para verificação: ");
+   numero = Convert.ToInt32(Console.ReadLine());
+
+   if (numero % 2 == 0)
+   {
+       Console.WriteLine("Número Par");
+   }
+   else
+   {
+       Console.WriteLine("Número Impar");
+   };
+        }
+    }
+}
