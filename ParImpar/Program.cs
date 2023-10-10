140b5d9605381a1288013aa310cc4c7a0a54e557
Kauã Fernandes <kauafernandes.smelo@gmail.com>
2023-10-10 14:00:49 -0300

    Atividade par e impar

---
+++ ParImpar/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um numero e verifique se ele é impar ou par!");
+Console.WriteLine("___________________________________________________\n");
+Console.Write("Seu numero: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 1) {
+    Console.WriteLine("O numero " + number + " é impar");
+} else {
+    Console.WriteLine("O numero " + number + " é par");
+}
