a0c15e14e4d571cebab09a9b4197c4a5764fb21e
Kaua <k1fernandes.melo@gmail.com>
2023-10-24 13:51:40 -0300

    Encadeamento

---
+++ EncadeamentoIFs/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Escreva sua nota: ");
+
+int nota = Convert.ToInt32(Console.ReadLine());
+String m;
+if(nota >= 9) {
+    m = "MB";
+} else if (nota >= 7) {
+    m = "B";
+} else if (nota >= 5) {
+    m = "R";
+} else {
+    m = "I";
+}
+
+Console.Write("Sua menção é: " + m);
+
